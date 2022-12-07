3d09dde53b27f937b2b99f2c687442a184926522
lucas de souza neri <lucas.neri121@hotmail.com>
2022-12-07 19:54:02 -0300

    'cors'

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -1,13 +1,30 @@
 const express = require('express')
 const authMiddleware = require('../middlewares/auth');
-const funcionario = require('../models/funcionario')
+const funcionario = require('../models/funcionario');
+const Project = require('../models/project');
+const Task = require('../models/task');
 
 const router = express.Router();
 
 router.use(authMiddleware);
 
-router.get('/',(req,res)=>{
-    res.send({ok:true, user: req.userId});
+router.get('/', async (req,res)=>{
+    res.send({user:req.userId})
+})
+router.get('/:projectId', async (req,res)=>{
+    res.send({user:req.userId})
+})
+
+router.post('/',(req,res)=>{
+    res.send({user: req.userId});
+})
+
+router.put('/:projectId', async (req,res)=>{
+    res.send({user:req.userId})
+})
+
+router.delete('/:projectId', async (req,res)=>{
+    res.send({user:req.userId})
 })
 
 module.exports = app => app.use('/projects',router);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 const express = require ('express');
 const bodyParser = require ('body-parser');
 const port = process.env.PORT || 3000;
-
+const cors = require('cors')
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
 
+app.use(cors({
+    origin: '*',
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}));
 
 require('./app/controllers/index')(app);
 
