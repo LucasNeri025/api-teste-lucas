2a9844fe24fca8bec7bdfeb00c1d02c76ce4f9ae
lucas de souza neri <lucas.neri121@hotmail.com>
2022-12-07 20:26:40 -0300

    'arrumandoCors'

--- src/index.js
+++ src/index.js
@@ -4,14 +4,20 @@ const port = process.env.PORT || 3000;
 const cors = require('cors')
 const app = express();
 
+
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin","*")
+    app.use(cors());
+    next()
+})
+
+
+
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
-
-app.use(cors({
-    origin: '*',
-    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
-}));
 
 require('./app/controllers/index')(app);
 
